86d4e5df30ea6bc4502b0e2b4edce4a52b103e1c
Johannes <johannes.belaschow@hs-osnabrueck.de>
2022-04-19 13:29:26 +0200

    tuples

--- intermediat_guide/tuples.py
+++ intermediat_guide/tuples.py
@@ -1,1 +1,36 @@
-print("hallo test test")
+# TUPLE IS IMMUTABLE
+
+test = ("test1", 2, "ers3")
+print(test)
+
+item = test[2]
+print(item)
+
+item1 = test[-1]
+print(item1)
+
+# test[0] ="kein test"
+
+for i in test:
+    print(i)
+
+if "test1" in test:
+    print("Johannes is bad in python")
+else:
+    print("Johannes is good in python")
+
+test1 = ("a","b","c","d")
+
+print(len(test1))
+
+print(test1.count("a"))
+
+
+
+
+
+
+
+
+
+
